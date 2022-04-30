a0ebf2be318e80cd8d5cb4caee443b467809ea03
purshotham <purshothamam@gmail.com>
2022-04-30 23:55:20 +0530

    React Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const buttons = ['CE', 'sin', 'cos', 'tan', 7, 8, 9, '-', 4, 5, 6, '+', 1, 2, 3, '*', '<<', '.', '/', '='];
+  const [value, setValue] = useState(0);
+  const [evaluated, setEvaluate] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>React Calculator</h2>
+        <div className='inputContainer'>
+          <input type="text" id='displayInput' readOnly value={value}/>
+        </div>
+        <div className="buttonsContainer">
+          {
+            buttons.map((item) => (
+              <button onClick={() => enterInput(item)}>{item}</button>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
+  
+  function enterInput(item) {
+    if(evaluated) {
+      setEvaluate(false)
+      setValue(value)
+    } 
+    
+    if(value === 0) {
+      console.log(value);
+      setValue(value + item);
+      console.log(value);
+    } else if(item === 'CE') {
+      setValue(0)
+    } else if(value[value.length - 1] === item){
+      return;
+    } else if(item === '='){
+      setValue(eval(value));
+      setEvaluate(true);
+    } else {
+      setValue(value.toString() + item)
+    }
+  }
 }
 
 export default App;
